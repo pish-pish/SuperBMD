7d8033b64ec756e8f82ac84895a88b0021fe50e2
Yoshi2 <debugyoshi@gmail.com>
2018-03-03 10:32:13 +0100

    Fix rigged characters getting messed up with loading and then saving a BMD

--- SuperBMD/source/BMD/EVP1.cs
+++ SuperBMD/source/BMD/EVP1.cs
@@ -88,7 +88,10 @@ namespace SuperBMD.BMD
                                                   reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle(),
                                                   reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
 
-                InverseBindMatrices.Add(new Matrix4(invBind.Row0, invBind.Row1, invBind.Row2, Vector4.UnitW));
+                Matrix4 invBind4 = new Matrix4(invBind.Row0, invBind.Row1, invBind.Row2, Vector4.UnitW);
+                invBind4.Transpose();
+
+                InverseBindMatrices.Add(invBind4);
             }
 
             reader.BaseStream.Seek(offset + evp1Size, System.IO.SeekOrigin.Begin);
