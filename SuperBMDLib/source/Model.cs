cd16414b53b1991e8a61f3734e30375f740d79b1
Yoshi2 <debugyoshi@gmail.com>
2019-08-31 20:29:17 +0200

    Add colon to list of characters to be sanitized in mat names

--- SuperBMDLib/source/Model.cs
+++ SuperBMDLib/source/Model.cs
@@ -23,7 +23,7 @@ namespace SuperBMDLib
         public MDL3 MatDisplayList    { get; private set; }
         public TEX1 Textures          { get; private set; }
         public BMDInfo ModelStats     { get; private set; }
-        private string[] characters_to_replace = new string[] { " ", "(", ")" };
+        private string[] characters_to_replace = new string[] { " ", "(", ")", ":" };
 
         private int packetCount;
         private int vertexCount;
