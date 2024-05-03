559dd942eb9a3640fa8424349f3a7c4556280e49
MrPishPIsh <74278948+pish-pish@users.noreply.github.com>
2024-05-02 23:07:33 -0400

    start base class for bck and bca

--- SuperBMDLib/source/Animation/J3DJointAnimation.cs
+++ SuperBMDLib/source/Animation/J3DJointAnimation.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using GameFormatReader.Common;
+using OpenTK;
+using SuperBMD.Util;
+using SuperBMDLib.Rigging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +23,103 @@ namespace SuperBMDLib.Animation
     {
         public string Name { get; private set; }
         public LoopMode LoopMode;
-        public byte RotationFrac;
+        public byte RotationScale;
         public short Duration;
 
+        protected string Magic;
+
         public Track[] Tracks;
+
+        public J3DJointAnimation(Assimp.Animation src_anim, List<Bone> bone_list)
+        {
+            Name = src_anim.Name;
+            LoopMode = LoopMode.Loop;
+            RotationFrac = 0;
+            Duration = (short)(src_anim.DurationInTicks * 30.0f);
+
+            Tracks = new Track[bone_list.Count];
+
+            for (int i = 0; i < bone_list.Count; i++)
+            {
+                Assimp.NodeAnimationChannel node = src_anim.NodeAnimationChannels.Find(x => x.NodeName == bone_list[i].Name);
+
+                if (node == null)
+                {
+                    Tracks[i] = Track.Identity(bone_list[i].TransformationMatrix, Duration);
+                }
+                else
+                {
+                    Tracks[i] = new Track()
+                    {
+                        Translation = GenerateTrack(node.PositionKeys, bone_list[i]),
+                        Rotation    = GenerateRotationTrack(node.RotationKeys, bone_list[i]),
+                        Scale       = GenerateTrack(node.ScalingKeys, bone_list[i]),
+                    };
+                }
+            }
+        }
+
+        public J3DJointAnimation(EndianBinaryReader reader) 
+        {
+            Magic = new string(reader.ReadChars(8));
+            reader.ReadInt32(); // filesize
+            reader.ReadInt32(); // section count
+            reader.Skip(16); 
+        }
+
+        private Axis GenerateTrack(List<Assimp.VectorKey> keys, Bone bone)
+        {
+            Axis axis = new Axis()
+            {
+                X = new Keyframe[keys.Count],
+                Y = new Keyframe[keys.Count],
+                Z = new Keyframe[keys.Count],
+            };
+
+            for (int i = 0; i < keys.Count; i++)
+            {
+                Assimp.VectorKey current_key = keys[i];
+                Vector3 value = new Vector3(current_key.Value.X, current_key.Value.Y, current_key.Value.Z);
+
+                axis.X[i].Key  = value.X;
+                axis.X[i].Time = (float)current_key.Time;
+
+                axis.Y[i].Key  = value.Y;
+                axis.Y[i].Time = (float)current_key.Time;
+
+                axis.Z[i].Key  = value.Z;
+                axis.Z[i].Time = (float)current_key.Time;
+            }
+
+            return axis;
+        }
+
+        private Axis GenerateRotationTrack(List<Assimp.QuaternionKey> keys, Bone bone)
+        {
+            Axis axis = new Axis()
+            {
+                X = new Keyframe[keys.Count],
+                Y = new Keyframe[keys.Count],
+                Z = new Keyframe[keys.Count],
+            };
+
+            for (int i = 0; i < keys.Count; i++)
+            {
+                Assimp.QuaternionKey current_key = keys[i];
+                Quaternion value = new Quaternion(current_key.Value.X, current_key.Value.Y, current_key.Value.Z, current_key.Value.W);
+                Vector3 quat_as_vec = QuaternionExtensions.ToEulerAngles(value);
+
+                axis.X[i].Key  = quat_as_vec.X;
+                axis.X[i].Time = (float)current_key.Time;
+
+                axis.Y[i].Key  = quat_as_vec.Y;
+                axis.Y[i].Time = (float)current_key.Time;
+
+                axis.Z[i].Key  = quat_as_vec.Z;
+                axis.Z[i].Time = (float)current_key.Time;
+            }
+
+            return axis;
+        }
     }
 }
--- SuperBMDLib/source/Animation/Track.cs
+++ SuperBMDLib/source/Animation/Track.cs
@@ -22,46 +22,53 @@ namespace SuperBMDLib.Animation
         public float OutTangent;
     }
 
+    public struct Axis
+    {
+        public Keyframe[] X;
+        public Keyframe[] Y;
+        public Keyframe[] Z;
+    }
+
     public struct Track
     {
-        public Keyframe[][] Translation;
-        public Keyframe[][] Rotation;
-        public Keyframe[][] Scale;
+        public Axis Translation;
+        public Axis Rotation;
+        public Axis Scale;
 
         public bool IsIdentity;
 
         public static Track Identity(Matrix4 Transform, float MaxTime)
         {
             Track ident_track = new Track();
-            Quaternion XRot = Quaternion.FromAxisAngle(Vector3.UnitX, (float)(0));
+            Quaternion XRot = Quaternion.FromAxisAngle(Vector3.UnitX, 0.0f);
 
             ident_track.IsIdentity = true;
             Vector3 Translation = Transform.ExtractTranslation();
 
-            ident_track.Translation = new Keyframe[][]
+            ident_track.Translation = new Axis()
             {
-                new Keyframe[] { new Keyframe() { InTangent = 0, Key = Translation.X, OutTangent = 0 } },
-                new Keyframe[] { new Keyframe() { InTangent = 0, Key = Translation.Y, OutTangent = 0, Time = 0} },
-                new Keyframe[] { new Keyframe() { InTangent = 0, Key = Translation.Z, OutTangent = 0, Time = 0} },
+                X = new Keyframe[] { new Keyframe() { InTangent = 0, Key = Translation.X, OutTangent = 0 } },
+                Y = new Keyframe[] { new Keyframe() { InTangent = 0, Key = Translation.Y, OutTangent = 0, Time = 0} },
+                Z = new Keyframe[] { new Keyframe() { InTangent = 0, Key = Translation.Z, OutTangent = 0, Time = 0} },
             };
 
             Quaternion Rotation = Transform.ExtractRotation();
             Vector3 Rot_Vec = QuaternionExtensions.ToEulerAngles(Rotation);
 
-            ident_track.Rotation = new Keyframe[][]
+            ident_track.Rotation = new Axis()
             {
-                new Keyframe[] { new Keyframe() { InTangent = 0, Key = Rot_Vec.X, OutTangent = 0, Time = 0} },
-                new Keyframe[] { new Keyframe() { InTangent = 0, Key = Rot_Vec.Y, OutTangent = 0, Time = 0} },
-                new Keyframe[] { new Keyframe() { InTangent = 0, Key = Rot_Vec.Z, OutTangent = 0, Time = 0} },
+                X = new Keyframe[] { new Keyframe() { InTangent = 0, Key = Rot_Vec.X, OutTangent = 0, Time = 0} },
+                Y = new Keyframe[] { new Keyframe() { InTangent = 0, Key = Rot_Vec.Y, OutTangent = 0, Time = 0} },
+                Z = new Keyframe[] { new Keyframe() { InTangent = 0, Key = Rot_Vec.Z, OutTangent = 0, Time = 0} },
             };
 
             Vector3 Scale = Transform.ExtractScale();
 
-            ident_track.Scale = new Keyframe[][]
+            ident_track.Scale = new Axis()
             {
-                new Keyframe[] { new Keyframe() { InTangent = 0, Key = 1, OutTangent = 0, Time = 0} },
-                new Keyframe[] { new Keyframe() { InTangent = 0, Key = 1, OutTangent = 0, Time = 0} },
-                new Keyframe[] { new Keyframe() { InTangent = 0, Key = 1, OutTangent = 0, Time = 0} },
+                X = new Keyframe[] { new Keyframe() { InTangent = 0, Key = 1, OutTangent = 0, Time = 0} },
+                Y = new Keyframe[] { new Keyframe() { InTangent = 0, Key = 1, OutTangent = 0, Time = 0} },
+                Z = new Keyframe[] { new Keyframe() { InTangent = 0, Key = 1, OutTangent = 0, Time = 0} },
             };
 
             return ident_track;
